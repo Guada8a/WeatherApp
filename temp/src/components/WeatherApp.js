f6223b7b3679654740d927704fcb286e3d996a4d
Guadalupe Ochoa <jochoa10@ucol.mx>
2022-11-30 18:30:47 -0600

    Best UI

--- temp/src/components/WeatherApp.js
+++ temp/src/components/WeatherApp.js
@@ -5,22 +5,21 @@ import nublado from '../img/nublado.jpeg';
 import nieve from '../img/nieve.jpg';
 import lluvia from '../img/lluvioso.jpg';
 
-const WeatherApp = ({ dt, temp, temp_min, temp_max, main, icon, weather }) => {
+const WeatherApp = ({ dt, temp, temp_min, temp_max, main, weather, sensation,description }) => {
 
     const date = new Date(dt);
     const date_format = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear().toString().substr(-2)}`;
     const day = date.toLocaleDateString('es-ES', { weekday: 'long' });
     const time = date.toLocaleTimeString('es-ES', { hour: '2-digit', minute: '2-digit' });
-    let clima = main === "Clear" ? "Despejado" : main === "Clouds" ? "Nublado" : main === "Rain" ? "Lluvioso" : main === "Snow" ? "Nieve" : "Clima Desconocido";
-    let icono = clima === "Despejado" ? soleado : clima === "Nublado" ? nublado : clima === "Lluvioso" ? lluvia : clima === "Nieve" ? nieve : "./img/interrogacion.png";
-    
+    let clima = main === "Clear" ? "Despejado" : main === "Clouds" ? "Nublado" : main === "Rain" ? "Lluvia" : main === "Snow" ? "Nieve" : "Clima Desconocido";
+    let icono = clima === "Despejado" ? soleado : clima === "Nublado" ? nublado : clima === "Lluvia" ? lluvia : clima === "Nieve" ? nieve : "./img/interrogacion.png";
+    let descripcion = description === "clear sky" ? "Cielo Despejado" : description === "few clouds" ? "Pocas Nubes" : description === "scattered clouds" ? "Nubes Dispersas" : description === "broken clouds" ? "Nubes Ruptas" : description === "shower rain" ? "Lluvia Escasa" : description === "rain" ? "Lluvia" : description === "thunderstorm" ? "Tormenta" : "light rain" ? "Lluvia":description === "snow" ? "Nieve" : description === "mist" ? "Niebla" : "Clima Desconocido";
 
     return (
-        <Card style={{ width: '15rem' }}>
+        <Card style={{ width: '15rem', borderRadius: '30px', boxShadow: '0 0 10px 0 rgba(0, 0, 0, 0.2)'}}>
             <Card.Img variant="left" src={icono} />
             <Card.Body>
                 <div style={{ textAlign: 'center' }}>
-
                     <Row>
                         <Col style={{ marginRight: '-2.6rem' }}>
                             <Row>
@@ -38,10 +37,18 @@ const WeatherApp = ({ dt, temp, temp_min, temp_max, main, icon, weather }) => {
                             </Row>
                         </Col>
                     </Row>
+                    <Row>
+                        <Col>
+                            <b>Sensación Térmica: </b>{Math.floor(sensation - 273.15, 2)} <sup>°C</sup>
+                        </Col>
+                    </Row>
                 </div>
                 <Card.Title>{clima}</Card.Title>
+                <Card.Text>
+                    {descripcion}
+                </Card.Text>
                 <p>
-                    {day}{' '}{date_format}{' '}{time}
+                    {day.substring(-1,3)}{' '}{date_format}{' '}{time}
                 </p>
             </Card.Body>
         </Card>
